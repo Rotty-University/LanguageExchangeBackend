20dad1996a77f56ef3dfc011f8ac724dc341fa31
zjk97 <zhangjake97@gmail.com>
2021-01-23 22:09:37 -0500

    finished JWT auth configs
    working on password encryption and attaching mongoDB next

--- src/main/java/com/rottyuniversity/languageexchange/LanguageexchangeApplication.java
+++ src/main/java/com/rottyuniversity/languageexchange/LanguageexchangeApplication.java
@@ -3,6 +3,7 @@ package com.rottyuniversity.languageexchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -10,11 +11,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class LanguageexchangeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LanguageexchangeApplication.class, args);
+
+		BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
+		String p = pe.encode("hello");
+		System.out.println(p);
+		System.out.println(pe.matches("hello", p));
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 }
--- src/main/java/com/rottyuniversity/languageexchange/controller/HomeController.java
+++ src/main/java/com/rottyuniversity/languageexchange/controller/HomeController.java
@@ -1,13 +1,63 @@
 package com.rottyuniversity.languageexchange.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.rottyuniversity.languageexchange.model.JwtRequest;
+import com.rottyuniversity.languageexchange.model.JwtResponse;
+import com.rottyuniversity.languageexchange.service.UserService;
+import com.rottyuniversity.languageexchange.utility.JWTUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeController {
 
+    @Autowired
+    private JWTUtility jwtUtility;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/")
     public String home() {
         return "You are logged in, Chungus God!";
+    }
+
+    @PostMapping("/authenticate")
+    public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    jwtRequest.getUsername(),
+                    jwtRequest.getPassword()
+            ));
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID CREDENTIALS", e);
+        }
+
+        final UserDetails userDetails = userService.loadUserByUsername(jwtRequest.getUsername());
+
+        final String token = jwtUtility.generateToken(userDetails);
+
+        return new JwtResponse(token);
+    }
+
+    @GetMapping("/testReq")
+    public String testReq(@RequestParam String who) throws Exception {
+        return "Hello, " + who;
+    }
+
+    @GetMapping("/testPath/{who}")
+    public String testPath(@PathVariable String who) throws Exception {
+        return "Hello, " + who;
+    }
+
+    @GetMapping("/testThree/{who}")
+    public String testThree(@PathVariable ("who") String who) throws Exception {
+        return "Hello, " + who;
     }
 }
