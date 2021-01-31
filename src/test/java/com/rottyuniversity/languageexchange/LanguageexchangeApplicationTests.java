6dadab32319f73f8c12e3558444376a870e35a46
zjk97 <zhangjake97@gmail.com>
2021-01-30 19:39:52 -0500

    built for deployment

--- src/test/java/com/rottyuniversity/languageexchange/LanguageexchangeApplicationTests.java
+++ src/test/java/com/rottyuniversity/languageexchange/LanguageexchangeApplicationTests.java
@@ -1,9 +1,7 @@
 package com.rottyuniversity.languageexchange;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class LanguageexchangeApplicationTests {
 
 	@Test
